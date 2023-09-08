d736e47b517025e93a831e8e983b65e976469827
yessygc <yessgarciacampos@gmail.com>
2023-09-07 22:14:06 -0600

    Creando proyecto y creando los primeros componentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoaderComponent } from './components/loader/loader.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidenavComponent,
+    HeaderComponent,
+    LoaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
